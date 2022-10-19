eafd3d15b5191c2dfdb57925e6e69cd5c641e600
Ingrida425 <98320957+Ingrida425@users.noreply.github.com>
2022-10-19 22:16:57 +0200

    trying to set api key

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,8 +1,17 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./DailyForecast.css";
+import { Axios } from "axios";
 
 export default function DailyForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = `a2dda52dce059eb8a14e95aaa0db6ab7`;
+  let longitude = -9.1;
+  let latitude = 38.7;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  Axios.get(apiUrl).then(handleResponse);
   return (
     <div className="DailyForecast">
       <div className="row">
